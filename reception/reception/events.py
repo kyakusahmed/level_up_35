90b00870de9165a13eeef40644b5d6451e14ea0f
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-09-01 01:42:18 +0300

    register_check --code--updates

--- reception/reception/events.py
+++ reception/reception/events.py
@@ -1,11 +1,10 @@
 file = open('VIP.txt','r')
 
-
 file1 = open('Ord.txt','r')
-
 
 ORD = []
 VIP = []
+
 
 for line in file:
     VIP.append(line)
@@ -13,19 +12,23 @@ for line in file1:
     ORD.append(line)
 
 
-
 def register_check(name,data):
     result = False
     for fullname in data:
-        if fullname.casefold().find(name.casefold()) != -1:nhg
-            result = fullname
-
-    return result 
+        if fullname.casefold().find(name.casefold()) != -1:
+            result = fullname 
+    
+    
+    return result
 
 while True:
     name1 = input("enter your name! ")
        
     print(register_check(name1,VIP))
+
+    print(register_check(name1,ORD))
+
+
 
 
 
