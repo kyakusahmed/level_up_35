3cc06efa1db5f011d43c741268a48f7b181c9452
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-09-06 17:19:24 +0300

    feat(API)
    API get all,add,delete routes
    
    [delivers #160283256]

---
+++ OOP/api2.py
@@ -0,0 +1,34 @@
+class Person:
+    
+
+    def __init__(self,name, height, age, ):
+
+        self.name = name
+        self.height = heoght
+        self.age = age 
+        
+        self.dict_of_students = {}
+
+    def add_student(self,student):
+        if(self.student_exists(student.id))
+        pass
+        self.dict_of_[student.id] = student
+
+    def find_student_by_id(self,id):
+        if(self.student_exists(id))
+        return self.dict_of_students[id]
+        
+
+    def delete_student(self,id):
+        pass
+
+    
+                
+
+
+class Student:
+    def __init__(self,id,name.age):
+        self.names = name
+
+
+
